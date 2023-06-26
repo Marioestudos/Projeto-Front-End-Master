c5477dab116259db2842676db19647467fdabd90
Mario Signorelligit config --global user.email signorelii2308@gmail.com <signorelii2308@gmail.com>
2023-06-26 20:54:38 -0300

    initial projec canvas

---
+++ canvas/canvas.js
@@ -0,0 +1,18 @@
+const canvasE1 = document.querySelector("canvas"),
+canvasCtx = canvasE1.getContext("2d");
+
+// fuctions for elemet canavas 
+function setup()
+    {
+        canvasE1.width = window.innerWidth;
+        canvasCtx.width = window.innerWidth;
+
+        canvasE1.height = window.innerHeight ;
+        canvasCtx.height = window.innerHeight ;
+
+    }
+function draw()
+{
+
+}
+setup()
