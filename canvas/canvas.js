cfcd10fe8806668f1a8a3754bd345fa4faae47c1
Mario Signorelligit config --global user.email signorelii2308@gmail.com <signorelii2308@gmail.com>
2023-06-29 20:02:58 -0300

    field and racket drawing

--- canvas/canvas.js
+++ canvas/canvas.js
@@ -1,6 +1,6 @@
 const canvasE1 = document.querySelector("canvas"),
 canvasCtx = canvasE1.getContext("2d");
-
+const linecernter = 15
 // fuctions for elemet canavas 
 function setup()
     {
@@ -13,6 +13,20 @@ function setup()
     }
 function draw()
 {
+    canvasCtx.fillStyle = "#286047"
+    canvasCtx.fillRect(0,0 ,window.innerWidth, window.innerHeight)
+    // line center
+    canvasCtx.fillStyle ="#fff"
+    const x = window.innerWidth /2 -linecernter /2;
+    const y = 0;
+    const w = linecernter;
+    const h = window.innerHeight;
+    canvasCtx.fillRect(x,y, w, h)
 
+    // left recket drawing
+    canvasCtx.fillRect(10,300,linecernter,200)
+    //rigth racket drawing
+    canvasCtx.fillRect( window.innerWidth -linecernter - 10,150,linecernter,200)
 }
 setup()
+draw();
